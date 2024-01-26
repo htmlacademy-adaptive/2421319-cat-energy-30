eed893f7cc0a5c6ea8474e04390f1f3c3c7e338d
VaselisaM <ralkina.vaselisa@gmail.com>
2024-01-26 12:46:26 +0500

    Адаптивная сетка

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+let navPage = document.querySelector('.page-navigation');
+let navButton = document.querySelector('.page-header__button');
+
+navPage.classList.remove('page-navigation--nojs');
+
+navButton.addEventListener('click', function () {
+  if (navPage.classList.contains('page-navigation--closed')) {
+    navPage.classList.remove('page-navigation--closed');
+    navPage.classList.add('page-navigation--opened');
+  } else {
+    navPage.classList.add('page-navigation--closed');
+    navPage.classList.remove('page-navigation--opened');
+  }
+});
